a96c23c5aa49896e8735331597b8ec0de556d13b
DogSniffers <DogSniffers@users.noreply.github.com>
2020-06-11 20:23:01 -0600

    more work on profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Button from '@material-ui/core/Button'
+import Input from '@material-ui/core/Input'
 import {connect} from 'react-redux'
 
 function Profile(props){
@@ -7,23 +8,52 @@ function Profile(props){
     const [age,setAge] = useState(null)
     const [feet,setFeet] = useState(null)
     const [inches,setInches] = useState(null)
+    const [weight,setWeight] = useState(null)
 
     function editView(){
         setEdit(!edit)
-        setAge(props.reduxState.age)
-        if(props.reduxState.feet === null){
-            setFeet(5)
-
+        if(props.reduxState.age){
+            setAge(props.reduxState.age)
         }else{
-            setFeet(props.reduxState.feet)
+            setAge(18)
         }
-        if(props.reduxState.inches === null){
-            setInches(0)
+        if(props.reduxState.feet){
+            setFeet(props.reduxState.feet)
         }else{
+            setFeet(5)
+        }
+        if(props.reduxState.inches){
             setInches(props.reduxState.inches)
+        }else{
+            setInches(0)
+        }
+        if(props.reduxState.weight){
+            setWeight(props.reduxState.weight)
+        }else{
+            setWeight(150)
         }
     }
-
+    function adjustAgeUp(){
+        if(age != 100){
+            setAge(age + 1)
+        }
+    }
+    function adjustAgeDown(){
+        if(age != 18){
+            setAge(age - 1)
+        }
+    }
+    let ageInputField = e => {
+        if(e.target.value <= 100){
+            if(e.target.value >= 18){
+                setAge(e.target.value)
+            }else{
+                setAge(18)
+            }
+        }else{
+            setAge(100)
+        }
+    }
     function adjustFeetUp(){
         if(feet != 9){
             setFeet(feet + 1 )
@@ -65,14 +95,23 @@ function Profile(props){
                 <p>Username:{props.reduxState.username}</p>
                 <p>Age:{props.reduxState.age}</p>
                 <p>Height:{props.reduxState.feet}{props.reduxState.inches}</p>
+                {props.reduxState.weight ? (
+                    <p>Weight:{props.reduxState.weight}</p>
+                ):(
+                    <p>No weight associated</p>
+                )}
                 <p onClick={() => editView()}>Edit</p>
-                <p>Normal View</p>
             </div>
             ):(
             <div>
                 <p>Username:{props.reduxState.username}</p>
                 <div>
-                    <p>Age:{props.reduxState.age}</p>
+                    <p>Age:{age}</p>
+                    <div>
+                        <button onClick={() => adjustAgeDown()}>Down</button>
+                        <Input onChange={ageInputField}/>
+                        <button onClick={() => adjustAgeUp()}>Up</button>
+                    </div>
                 </div>
                 <div>
                     <p>Feet:{feet}</p>
@@ -84,8 +123,11 @@ function Profile(props){
                     <button onClick={() => adjustInchesDown()}>Down</button>
                     <button onClick={() => adjustInchesUp()}>Up</button>
                 </div>
+                <div>
+                    <p>Weight:</p>
+                    <Input/>
+                </div>
                 <p onClick={() => editView()}>Edit</p>
-                <p>Edit Mode</p>
             </div>
             )}
         </div>
